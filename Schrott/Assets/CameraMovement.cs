54943edb0f9609986357fdad67b5ca2e5292091f
Amon Kalagin <amon.kalagin@gmail.com>
2015-06-10 12:28:30 +0200

    Die + Spawn crates mechanic

---
+++ Schrott/Assets/CameraMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	// Use this for initialization
+	public 
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = this.transform.position;
+		position.y += 0.03f;
+		this.transform.position = position;
+	}
+}
---
+++ Schrott/Assets/spawn.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawn : MonoBehaviour {
+	// Use this for initialization
+	public GameObject crate;
+	void Spawn () {
+		GameObject crate =(GameObject)Instantiate((GameObject)Resources.Load("crate"));
+	}
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Spawn ();
+	}
+}
