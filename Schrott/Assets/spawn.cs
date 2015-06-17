be064337c0603ac4b23e5d6f46fb81297c1fcd98
Amon Kalagin <amon.kalagin@gmail.com>
2015-06-17 09:33:56 +0200

    Basic trash spawn added

--- Schrott/Assets/spawn.cs
+++ Schrott/Assets/spawn.cs
@@ -4,16 +4,21 @@ using System.Collections;
 public class spawn : MonoBehaviour {
 	// Use this for initialization
 	public GameObject crate;
+	public float spawnTime = 1f;
 	void Spawn () {
+
 		GameObject crate =(GameObject)Instantiate((GameObject)Resources.Load("crate"));
+		crate.transform.position = this.transform.position;
 	}
 	void Start () {
+		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+		//Invoke("Spawn", spawnTime);
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-		Spawn ();
+
 	}
 }
